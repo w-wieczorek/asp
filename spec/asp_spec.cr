d0607fd783d705abcf22cc1a8127da431faf4060
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2021-07-21 22:18:47 +0200

    First tests

--- spec/asp_spec.cr
+++ spec/asp_spec.cr
@@ -1,9 +1,13 @@
-require "./spec_helper"
+require "spec"
+require "../src/asp.cr"
 
 describe Asp do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "creates a rule and adds it to a logic program" do
+    model = Asp::Program.new
+    p = Asp::Atom.new (0..3)
+    model.addRule p[1], ~p[2], ~p[3], implies: p[0]
+    model.atoms.should eq(Set{p[0].atom, p[1].atom, p[2].atom, p[3].atom})
+    model.rules.size.should eq(1)
+    model.rules.should contain({head: p[0].atom, positives: Set{p[1].atom}, negatives: Set{p[2].atom, p[3].atom}})
   end
 end
--- src/asp.cr
+++ src/asp.cr
@@ -1,32 +1,75 @@
 module Asp
   alias AtomId = Tuple(UInt64, UInt64)
-  alias Literal = NamedTuple(atom: AtomId, negated : Bool)
-  alias Rule = NamedTuple(head: Literal, body: Set(Literal))
+  alias Rule = NamedTuple(head: AtomId?, positives: Set(AtomId), negatives: Set(AtomId))
 
-  class Atom
-    def initialize(@indices : Enumerable(U)) forall U
+  struct Literal
+    property atom : AtomId
+    property negated : Bool
+
+    def initialize(@atom, @negated)
     end
 
-    def []=(head, *body) : Rule
-    end
-
-    def [](idx)
-      {self.object_id, idx.hash}
+    def ~
+      Literal.new(@atom, true)
     end
   end
 
-  class Rule
+  class Atom
+    property indices : Array(UInt64)
+
+    def initialize(range : Range(Int32, Int32))
+      @indices = range.to_a.map &.hash
+    end
+
+    def initialize(range : Array(T)) forall T
+      @indices = range.to_a.map &.hash
+    end
+
+    def initialize(range : Set(T)) forall T
+      @indices = range.to_a.map &.hash
+    end
+
+    def [](idx : Object) : Literal
+      Literal.new({self.object_id, idx.hash}, false)
+    end
   end
 
   class Program
     include Iterator(Set(AtomId))
 
+    getter atoms
+    getter rules
+
     def initialize
       @solution_procedure_started = false
+      @atoms = Set(AtomId).new
+      @rules = Set(Rule).new
+    end
+
+    def addRule(*body, implies head)
+      raise ArgumentError.new("The head cannot be negated!") if head.negated
+      pos_set = Set(AtomId).new
+      neg_set = Set(AtomId).new
+      body.each do |literal|
+        if literal.negated
+          neg_set.add(literal.atom)
+        else
+          pos_set.add(literal.atom)
+        end
+      end
+      @rules.add({head: head.atom, positives: pos_set, negatives: neg_set})
+      @atoms.add(head.atom)
+      @atoms.concat(pos_set)
+      @atoms.concat(neg_set)
     end
 
     def next
-      @solution_procedure_started = true
+      if !@solution_procedure_started
+        @solution_procedure_started = true
+        Set(AtomId).new
+      else
+        Stop
+      end
     end
   end
 end
