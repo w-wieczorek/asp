b95cbfb4f574783dcee0bed71b71a0f2746f88e3
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2021-07-21 15:16:53 +0200

    Some classes - the first code

--- src/asp.cr
+++ src/asp.cr
@@ -1,6 +1,32 @@
-# TODO: Write documentation for `Asp`
 module Asp
-  VERSION = "0.1.0"
+  alias AtomId = Tuple(UInt64, UInt64)
+  alias Literal = NamedTuple(atom: AtomId, negated : Bool)
+  alias Rule = NamedTuple(head: Literal, body: Set(Literal))
 
-  # TODO: Put your code here
+  class Atom
+    def initialize(@indices : Enumerable(U)) forall U
+    end
+
+    def []=(head, *body) : Rule
+    end
+
+    def [](idx)
+      {self.object_id, idx.hash}
+    end
+  end
+
+  class Rule
+  end
+
+  class Program
+    include Iterator(Set(AtomId))
+
+    def initialize
+      @solution_procedure_started = false
+    end
+
+    def next
+      @solution_procedure_started = true
+    end
+  end
 end
