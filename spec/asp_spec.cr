896f9e3e7d9379e25182a3918392e9652b4dffc2
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2021-07-22 16:09:06 +0200

    Reduct in progress

--- spec/asp_spec.cr
+++ spec/asp_spec.cr
@@ -4,10 +4,53 @@ require "../src/asp.cr"
 describe Asp do
   it "creates a rule and adds it to a logic program" do
     model = Asp::Program.new
-    p = Asp::Atom.new (0..3)
+    p = Asp::LiteralFactory.new (0..3)
     model.addRule p[1], ~p[2], ~p[3], implies: p[0]
     model.atoms.should eq(Set{p[0].atom, p[1].atom, p[2].atom, p[3].atom})
     model.rules.size.should eq(1)
-    model.rules.should contain({head: p[0].atom, positives: Set{p[1].atom}, negatives: Set{p[2].atom, p[3].atom}})
+    model.rules.should contain({head: p[0].atom, positives: Set{p[1].atom}, 
+      negatives: Set{p[2].atom, p[3].atom}})
+  end
+
+  it "adds facts and constraints to a logic program" do
+    model = Asp::Program.new
+    names = ["Ala", "Ewa", "Adam"]
+    idx_arr = Array({Int32, String}).new(3) { |i| {i, names[i]} }
+    person = Asp::LiteralFactory.new idx_arr
+    model.addFact person[{0, "Ala"}]
+    model.addConstraint person[{1, "Ewa"}], ~person[{2, "Adam"}]
+    model.atoms.should eq(Set{person[{0, "Ala"}].atom, person[{1, "Ewa"}].atom, 
+      person[{2, "Adam"}].atom, Asp::DUMMY.atom})
+    model.rules.size.should eq(2)
+    r1 = {head: person[{0, "Ala"}].atom, positives: Asp::EMPTY, 
+      negatives: Asp::EMPTY}
+    model.rules.should contain(r1)
+    r2 = {head: Asp::DUMMY.atom, positives: Set{person[{1, "Ewa"}].atom}, 
+      negatives: Set{person[{2, "Adam"}].atom, Asp::DUMMY.atom}}
+    model.rules.should contain(r2)
+  end
+
+  it "computes reduct" do
+    model = Asp::Program.new
+    idx_arr = [:a, :b]
+    idx_arr2 = Array(Symbol).new(4)
+    idx.arr.each do |i|
+      idx_arr.each { |j| idx_arr2 << {i, j} }
+    end
+    el = Asp::LiteralFactory.new idx_arr
+    equal = Asp::LiteralFactory.new idx_arr2
+    neq = Asp::LiteralFactory.new idx_arr2
+    idx_arr.each do |i|
+      model.addFact el[i] 
+      model.addRule el[i], implies: equal[{i, i}]
+      idx_arr.each do |j|
+        model.addRule el[i], el[j], ~equal[{i, j}], implies: neq[{i, j}]
+      end
+    end
+    expected = Set(Rule).new
+    expected.add({head: el[:a].atom, positives: Asp::EMPTY, negatives: Asp::EMPTY})
+    expected.add({head: el[:b].atom, positives: Asp::EMPTY, negatives: Asp::EMPTY})
+    expected.add({head: equal[{:a, :a}].atom, positives: Set{el[:a].atom}, negatives: Asp::EMPTY})
+    expected.add({head: equal[{:b, :b}].atom, positives: Set{el[:b].atom}, negatives: Asp::EMPTY})
   end
 end
--- src/asp.cr
+++ src/asp.cr
@@ -1,9 +1,9 @@
 module Asp
-  alias AtomId = Tuple(UInt64, UInt64)
-  alias Rule = NamedTuple(head: AtomId?, positives: Set(AtomId), negatives: Set(AtomId))
+  alias Atom = Tuple(UInt64, UInt64)
+  alias Rule = NamedTuple(head: Atom, positives: Set(Atom), negatives: Set(Atom))
 
   struct Literal
-    property atom : AtomId
+    property atom : Atom
     property negated : Bool
 
     def initialize(@atom, @negated)
@@ -14,19 +14,34 @@ module Asp
     end
   end
 
-  class Atom
+  EMPTY = Set(Atom).new
+  DUMMY = Literal.new({0_u64, 0_u64}, false)
+  
+  class LiteralFactory
     property indices : Array(UInt64)
 
     def initialize(range : Range(Int32, Int32))
       @indices = range.to_a.map &.hash
+      @indices.uniq!
+      if @indices.size < range.size
+        raise RuntimeError.new("Inefficient hash function for class Int32.")
+      end
     end
 
     def initialize(range : Array(T)) forall T
-      @indices = range.to_a.map &.hash
+      @indices = range.map &.hash
+      @indices.uniq!
+      if @indices.size < range.size
+        raise RuntimeError.new("Inefficient hash function for class #{T.name}.")
+      end
     end
 
     def initialize(range : Set(T)) forall T
       @indices = range.to_a.map &.hash
+      @indices.uniq!
+      if @indices.size < range.size
+        raise RuntimeError.new("Inefficient hash function for class #{T.name}.")
+      end
     end
 
     def [](idx : Object) : Literal
@@ -35,21 +50,22 @@ module Asp
   end
 
   class Program
-    include Iterator(Set(AtomId))
+    include Iterator(Set(Atom))
 
     getter atoms
     getter rules
 
     def initialize
       @solution_procedure_started = false
-      @atoms = Set(AtomId).new
+      @atoms = Set(Atom).new
       @rules = Set(Rule).new
     end
 
     def addRule(*body, implies head)
       raise ArgumentError.new("The head cannot be negated!") if head.negated
-      pos_set = Set(AtomId).new
-      neg_set = Set(AtomId).new
+      raise RuntimeError.new("Cannot change model during solving!") if @solution_procedure_started
+      pos_set = Set(Atom).new
+      neg_set = Set(Atom).new
       body.each do |literal|
         if literal.negated
           neg_set.add(literal.atom)
@@ -63,13 +79,47 @@ module Asp
       @atoms.concat(neg_set)
     end
 
+    def addFact(head)
+      raise ArgumentError.new("The head cannot be negated!") if head.negated
+      raise RuntimeError.new("Cannot change model during solving!") if @solution_procedure_started
+      @rules.add({head: head.atom, positives: EMPTY, negatives: EMPTY})
+      @atoms.add(head.atom)
+    end
+
+    def addConstraint(*body)
+      raise RuntimeError.new("Cannot change model during solving!") if @solution_procedure_started
+      pos_set = Set(Atom).new
+      neg_set = Set(Atom).new
+      body.each do |literal|
+        if literal.negated
+          neg_set.add(literal.atom)
+        else
+          pos_set.add(literal.atom)
+        end
+      end
+      neg_set.add(DUMMY.atom)
+      @rules.add({head: DUMMY.atom, positives: pos_set, negatives: neg_set})
+      @atoms.concat(pos_set)
+      @atoms.concat(neg_set)
+    end
+
     def next
       if !@solution_procedure_started
         @solution_procedure_started = true
-        Set(AtomId).new
+        Set(Atom).new
       else
         Stop
       end
     end
+  end
+
+  def reduct(pi : Set(Rule), x : Set(Atom)) : Set(Rule)
+    result = Set(Rule).new
+    pi.each do |r|
+      unless x.intersects? r[:negatives] 
+        result.add({head: r[:head], positives: r[:positives], negatives: EMPTY})
+      end
+    end
+    result
   end
 end
